c43160cf3fc68700e2a65a6bd237c677b86c25b8
sawe-daisy <deesawe16@gmail.com>
2020-09-07 00:03:36 +0300

    more functions

--- js/script.js
+++ js/script.js
@@ -5,4 +5,35 @@ $("document").ready(function () {
     this.topping = topping;
     this.crust = crust;
   }
+  pizza.prototype.getPizzaPrice = function () {
+    return this.getCrustPrice() + this.getToppingPrice() + this.getTypePrice();
+  };
+
+  pizza.prototype.getToppingPrice = function () {
+    if (this.size === "large") {
+      if (this.topping === "onions") {
+        return 400;
+      } else if (this.topping === "mushrooms") {
+        return 450;
+      } else {
+        return 400;
+      }
+    } else if (this.size === "medium") {
+      if (this.topping === "onions") {
+        return 300;
+      } else if (this.topping === "mushrooms") {
+        return 350;
+      } else {
+        return 300;
+      }
+    } else {
+      if (this.topping === "onions") {
+        return 200;
+      } else if (this.topping === "mushrooms") {
+        return 300;
+      } else {
+        return 200;
+      }
+    }
+  };
 });
