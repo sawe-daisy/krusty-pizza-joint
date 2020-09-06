f34b77fb0b010e506cd320a3cced51390931e81d
sawe-daisy <deesawe16@gmail.com>
2020-09-07 00:01:15 +0300

    js functions

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$("document").ready(function () {
+  function pizza(type, size, topping, crust) {
+    this.type = type;
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+  }
+});
